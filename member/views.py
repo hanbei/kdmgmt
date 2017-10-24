fb7e178b53df428b930eda0f4e500a528119dff3
Florian Schulz <hanbei@hanbei.de>
2017-10-24 21:27:28 +0200

    Initial views for member listing and details

--- member/views.py
+++ member/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 # Create your views here.
+from .models import Member
+
+
+class ListView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'members'
+
+    def get_queryset(self):
+        return Member.objects.order_by('name')
+
+
+class DetailView(generic.DetailView):
+    model = Member
+    template_name = 'details.html'
